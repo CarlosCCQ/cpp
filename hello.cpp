9a93dbff6d4a85d39da19c31d4e4f94267a7cd70
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-06-18 23:17:49 -0500

    Agregando la variable last_name

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
-
+	String last_name="";
 	String first_name="";
-	cout << "What is your name?" << endl;
+	cout << "What is your first_name?" << endl;
 	cin >> first_name;
+	cout << "Whats is your last_name" << endl;
+	cin >> last_name;
 	cout << "Hello " << first_name << endl;
 	return 0;
 }
